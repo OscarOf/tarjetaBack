20113d24ca5e1e2779fe9a4d692240bf7ecd5e8f
Óscar O <oscarforjuela@hotmail.com>
2023-01-26 17:48:26 -0500

    Nuevos cambios

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hola");
+const express = require('express');
+const app = express();
+const PORT = 3030;
+
+app.get("/", (req, res)=> {//res: Es para enviar una respuesta, req: Es para tomar datos de los usuarios
+    res.send("Hola sirve");
+}); 
+
+app.listen(PORT, ()=>{
+    console.log("El puerto es:"+ PORT);
+});
